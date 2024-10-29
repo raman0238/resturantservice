68108dccb127f37744797914eef1c0d26de14f58
raman0238 <ramankumar0238@gmail.com>
2024-10-29 10:21:03 +0530

    resturantservice - added configs for making service a eureka client

--- src/main/java/com/example/resturantservice/ResturantserviceApplication.java
+++ src/main/java/com/example/resturantservice/ResturantserviceApplication.java
@@ -2,7 +2,9 @@ package com.example.resturantservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ResturantserviceApplication {
 
